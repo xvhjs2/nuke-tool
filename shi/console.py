27d770217bf6c8e85fdbfbf753d906c0fafec994
xvhjs2 <tramtram4400@gmail.com>
2025-10-20 21:52:39 -0400

    Update console.py

--- shi/console.py
+++ shi/console.py
@@ -82,15 +82,13 @@ class Logging:
 class Options:
     def options(self):
         self.opt = '''
-            [01] Delete all channels
-            [02] Edit the server
-            [03] Create channels
-            [04] Spam messages
-            [05] Spam messages with webhooks
-            [21] Select a different server
-            [41] Use a different bot
-            [67] Full nuke
-            [100] Exit
+            [01] Delete all channels            [07] Give admin
+            [02] Edit the server                [21] Select a different server
+            [03] Create channels                [41] Use a different bot
+            [04] Spam messages                  [67] Full nuke
+            [05] Webhook spam                   [100] Exit
+            [06] Create roles
+
         '''
         self.opti = Colorate.Horizontal(Colors.blue_to_purple, self.opt, 1)
         return self.opti
